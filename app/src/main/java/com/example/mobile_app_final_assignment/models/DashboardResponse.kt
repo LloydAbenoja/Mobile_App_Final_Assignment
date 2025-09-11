7fc79ece4429c686c2688810c4d17cfb8765fdae
Lloyd <lloyd.abenoja@gmail.com>
2025-09-11 21:09:11 +1000

    Revert "Fixing models, network and viewmodels"
    This reverts commit ee1ebaf0269bbdf5743357432575dd7eddabc614.

--- app/src/main/java/com/example/mobile_app_final_assignment/models/DashboardResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mobile_app_final_assignment.models
-
-import com.squareup.moshi.Json
-
-data class DashboardResponse(
-    @Json(name = "entities") val entities: List<ResponseItem>,
-    @Json(name = "entityTotal") val entityTotal: Int
-)
--- app/src/main/java/com/example/mobile_app_final_assignment/network/ApiDevService.kt
+++ app/src/main/java/com/example/mobile_app_final_assignment/network/ApiDevService.kt
@@ -2,23 +2,19 @@ package com.example.mobile_app_final_assignment.network
 
 import com.example.mobile_app_final_assignment.models.AddObjectRequest
 import com.example.mobile_app_final_assignment.models.AddObjectResponse
-import com.example.mobile_app_final_assignment.models.DashboardResponse
+import com.example.mobile_app_final_assignment.models.ResponseItem
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ApiDevService {
-    // Login: /{location}/auth
-    @POST("{location}/auth")
-    suspend fun login(
-        @Path("location") location: String,
-        @Body requestBody: AddObjectRequest
-    ): AddObjectResponse
+    @GET("objects")
+    suspend fun getAllObjects(): List<ResponseItem>
 
-    // Dashboard: /dashboard/{keypass}
-    @GET("dashboard/{keypass}")
-    suspend fun getDashboard(
-        @Path("keypass") keypass: String
-    ): DashboardResponse
+    @GET("objects/{number}")
+    suspend fun getSingleObject(@Path("number") id: Int): ResponseItem
+
+    @POST("objects")
+    suspend fun addObject(@Body requestBody: AddObjectRequest): AddObjectResponse
 }
